eac06381df78234d5f76a89ed26455953cccc406
HunaidV <31166049+HunaidV@users.noreply.github.com>
2024-03-05 20:15:08 +0000

    library

--- CS50_snippets/itunes.py
+++ CS50_snippets/itunes.py
@@ -5,7 +5,11 @@ import json
 if len(sys.argv) != 2:
     sys.exit()
 
-response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
+response = requests.get("https://itunes.apple.com/search?entity=song&limit=100&term=" + sys.argv[1])
 
-print(response.json())
+#print(json.dumps(response.json(), indent=2))
 
+o = response.json()
+
+for result in o["results"]:
+    print(result["trackName"])
---
+++ CS50_snippets/sayings.py
@@ -0,0 +1,17 @@
+
+
+def main():
+
+    hello("Hunaid")
+    goodbye("Hunaid") 
+
+def hello(name):
+    print("Hello", name)
+
+def goodbye(name):
+    print("Goodbye", name
+    )
+
+
+if __name__ == "__main__":
+    main()
