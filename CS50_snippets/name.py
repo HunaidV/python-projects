9ab3d65b1e2f3f67541772282cebec4f0d92f750
HunaidV <31166049+HunaidV@users.noreply.github.com>
2024-03-05 19:40:38 +0000

    cowsay package

--- CS50_snippets/name.py
+++ CS50_snippets/name.py
@@ -2,6 +2,22 @@ import os
 import sys
 #sys.arg system argument to provide before entering elements
 
+if len(sys.argv) < 2:
+    sys.exit("Too few arguments")
+
+
+for arg in sys.argv[1:-1]: #negative number goes from right to left and remove the right arguments
+    print("Hello, name is ", arg)
+
+
+
+
+# if len(sys.argv) < 2:
+#     sys.exit("Too few Args") #this will termninate the program if the input is not correct
+# elif len(sys.argv) < 2:
+#     sys.exit("Too many Arg")
+
+# print("Hello my name is ", sys.argv[1])
 
 
 
